b619bd1612c74527502f684c4803701f94585dd2
pvcresin <pvcresin0730@gmail.com>
2020-07-29 22:02:05 +0900

    Goobye babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	entry: './index.js',
 	output: {
 		path: path.join(__dirname, './'),
-		filename: 'index.js'
+		filename: 'index.js',
 	},
 	devtool: isDev ? 'inline-source-map' : undefined,
 	module: {
@@ -20,20 +20,14 @@ module.exports = {
 				exclude: /node_modules/,
 				use: {
 					loader: 'riot-tag-loader',
-					query: { hot: false, template: 'pug' }
-				}
+					query: {
+						hot: false,
+						template: 'pug',
+					},
+				},
 			},
-			{
-				test: /\.js|\.tag$/,
-				enforce: 'post',
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					query: { presets: ['es2015-riot'] }
-				}
-			}
-		]
+		],
 	},
 	resolve: { extensions: ['.js', '.tag'] },
-	plugins: isDev ? [] : [new BundleAnalyzerPlugin()]
+	plugins: isDev ? [] : [new BundleAnalyzerPlugin()],
 }
