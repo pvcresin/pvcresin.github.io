74a2c29f5249f7674153e68dc90a131a38cf3078
pvcresin <pvcresin0730@gmail.com>
2020-10-01 23:59:58 +0900

    Update base dir

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const baseDirectory = '/docs'
+
 module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? baseDirectory : '',
+  basePath: process.env.NODE_ENV === 'production' ? baseDirectory : '',
   trailingSlash: true,
   exportPathMap: async function () {
     const paths = {
