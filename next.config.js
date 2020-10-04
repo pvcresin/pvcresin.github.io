12195cb0715e2312549058faf1384f2def8e68ab
pvcresin <pvcresin0730@gmail.com>
2020-10-04 23:17:46 +0900

    Fix config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,4 @@ module.exports = {
   assetPrefix: process.env.NODE_ENV === 'production' ? baseDirectory : '',
   basePath: process.env.NODE_ENV === 'production' ? baseDirectory : '',
   trailingSlash: true,
-  exportPathMap: async function () {
-    const paths = {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-    }
-    return paths
-  },
 }
