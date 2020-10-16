809e6286f6d859b2ec91b093e33ea869592c8b90
pvcresin <pvcresin0730@gmail.com>
2020-10-17 00:45:38 +0900

    Add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from 'next'
 import Link from 'next/link'
 
+import Head from '../components/head'
 import Footer from '../components/footer'
 
 const IndexPage: NextPage<{}> = ({}) => (
   <div>
+    <Head />
     <h1>Index(Top)Page</h1>
     <p>
       <Link href='/about'>
