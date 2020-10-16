d5d7196a1d07056cd33419d33e387bfb5cef4320
pvcresin <pvcresin0730@gmail.com>
2020-10-17 00:29:18 +0900

    Impl Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,18 @@
-const Footer = () => <div>Footer</div>
+import dayjs from 'dayjs'
+
+import Social from './social'
+
+const Footer = () => {
+  const year = dayjs().format('YYYY')
+
+  return (
+    <div className='components-footer'>
+      <div className='container'>
+        <Social></Social>
+        <p>© {year} pvcresin</p>
+      </div>
+    </div>
+  )
+}
 
 export default Footer
