51c8877850bd024db1ef410ac1b724fba36ddbbe
pvcresin <pvcresin0730@gmail.com>
2020-10-17 14:55:07 +0900

    Fix attr name

--- components/head.tsx
+++ components/head.tsx
@@ -4,7 +4,7 @@ const Head = () => (
   <NextHead>
     <title>Raika Toriyama | pvcresin</title>
     <meta charSet='UTF-8' />
-    <meta http-equiv='X-UA-Compatible' content='ie=edge' />
+    <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <meta name='author' content='pvcresin' />
     <meta name='description' content='pvcresin portfolio' />
