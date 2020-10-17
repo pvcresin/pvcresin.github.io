2bfd747ff3eeca2c45ac93ffaca0fcd2d5d582f2
pvcresin <pvcresin0730@gmail.com>
2020-10-17 22:54:09 +0900

    Impl categiry filter function

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,41 +1,32 @@
-import projectsArray from '../src/data'
+import { useState } from 'react'
+
+import projectsArray from '../data'
 import Webp from './webp'
 
-const Projects = (props) => {
-  const category = 'All'
-  const projects = props.limit === undefined ? projectsArray : projectsArray.slice(0, props.limit)
+const initialCategory = 'All'
 
-  // this.on('updated', () => {
-  //   //- console.log(this.projects, props.category)
-
-  //   if (props.category && props.category !== category) {
-  //     this.update({
-  //       category: props.category,
-  //       projects:
-  //         props.category === 'All'
-  //           ? projectsArray
-  //           : projectsArray.filter((p) => p.category === props.category),
-  //     })
-  //   }
-  // })
+const Projects: React.FC<{ category: string; limit?: number }> = ({ category, limit }) => {
+  const projects = limit === undefined ? projectsArray : projectsArray.slice(0, limit)
 
   return (
     <div className='components-projects'>
-      {projects.map((w) => (
-        <div className='pure-u-sm-1-2 pure-u-md-1-3 pure-u-1-1' key={w.name}>
-          <div className='card'>
-            <a href='#projects/{w.name.replace(/ /g, `_`)}'>
-              <div className='wrapper'>
-                <Webp className='thumbnail' src={w.img}></Webp>
-              </div>
-              <div className='content'>
-                <h4>{w.name}</h4>
-                <p>{w.sub}</p>
-              </div>
-            </a>
+      {projects
+        .filter((p) => category === initialCategory || p.category === category)
+        .map((w) => (
+          <div className='pure-u-sm-1-2 pure-u-md-1-3 pure-u-1-1' key={w.name}>
+            <div className='card'>
+              <a href={`/projects/${w.name}`}>
+                <div className='wrapper'>
+                  <Webp className='thumbnail' src={w.img}></Webp>
+                </div>
+                <div className='content'>
+                  <h4>{w.name}</h4>
+                  <p>{w.sub}</p>
+                </div>
+              </a>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   )
 }
