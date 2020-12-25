4143c88733ed2e40aa8918be0177b5b5aa98c700
pvcresin <pvcresin0730@gmail.com>
2020-12-25 22:54:49 +0900

    Remove nojekill

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withSass = require('@zeit/next-sass')
 
-const basePath = '/docs'
+export const basePath = '/docs'
 
 module.exports = withSass({
   assetPrefix: process.env.NODE_ENV === 'production' ? basePath : '',
