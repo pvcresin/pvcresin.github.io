3777da46023ccc052abe0ac9a7c3f657aa1cec10
pvcresin <pvcresin0730@gmail.com>
2022-04-18 14:13:34 +0900

    Move link tag to document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,20 @@ import 'purecss/build/grids-responsive-min.css'
 
 import '../styles/index.scss'
 
-import Head from 'components/head'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: any) => (
   <>
-    <Head />
+    <Head>
+      <title>Raika Toriyama | pvcresin</title>
+      <meta charSet='UTF-8' />
+      <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta name='author' content='pvcresin' />
+      <meta name='description' content='pvcresin portfolio' />
+      <meta name='keywords' content='pvcresin,raika toriyama,鳥山 らいか,鳥山らいか' />
+      <meta name='theme-color' content='#1a1819' />
+    </Head>
     <Component {...pageProps} />
   </>
 )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document = () => (
   <Html lang='ja'>
-    <Head />
+    <Head>
+      <link rel='icon' href='/favicon.ico' />
+      <link rel='shortcut icon' href='/favicon.ico' />
+
+      {/* font */}
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Montserrat&family=Noto+Sans+JP&display=swap'
+        rel='stylesheet'
+      />
+
+      {/* polyfill */}
+      <script
+        crossOrigin='anonymous'
+        src='https://polyfill.io/v3/polyfill.min.js?features=default'
+        defer
+      ></script>
+    </Head>
     <body>
       <Main />
       <NextScript />
