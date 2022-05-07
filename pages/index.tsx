34e3209fc20cb92718fe9b1b51ea2ff52bdaccff
pvcresin <pvcresin0730@gmail.com>
2022-05-08 02:44:01 +0900

    Make dark projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import First from 'components/first'
 import About from 'components/about'
-import Projects from 'components/projects'
 import Awards from 'components/awards'
 import Publications from 'components/publications'
 import More from 'components/more'
@@ -17,13 +16,6 @@ const IndexPage = () => (
       <div className='container'>
         <About />
         <More text='detail' link='/about' />
-      </div>
-    </section>
-    <section className={styles.section}>
-      <div className='container'>
-        <h2 className='section-title'>Projects</h2>
-        <Projects limit={3} />
-        <More link='/projects' />
       </div>
     </section>
     <section className={styles.section}>
