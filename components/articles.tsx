d765f88baba9ca91988a7efcfcdba471280d1a55
pvcresin <pvcresin0730@gmail.com>
2022-05-07 18:37:00 +0900

    Add zenn qiita articles

--- components/articles.tsx
+++ components/articles.tsx
@@ -8,6 +8,12 @@ const news: NewsData[] = [
     link: 'https://buildersbox.corp-sansan.com/entry/2022/04/19/110000',
   },
   {
+    date: '2021-12-21',
+    title: 'Hydro-SDK で始める TypeScript を使った Flutter 開発',
+    siteName: 'Zenn',
+    link: 'https://zenn.dev/pvcresin/articles/7cc9df5d4e23f5',
+  },
+  {
     date: '2021-11-04',
     title: 'GIF しか残ってなかったオリジナルのローディングを CSS アニメーションで再現した話',
     siteName: 'Sansan Builders Blog',
@@ -38,6 +44,19 @@ const news: NewsData[] = [
     link: 'https://buildersbox.corp-sansan.com/entry/2021/02/12/110000',
   },
   {
+    date: '2020-12-29',
+    title: 'TypeScript の Conditional Types で複数条件をマージする',
+    siteName: 'Zenn',
+    link: 'https://zenn.dev/pvcresin/articles/073dc774eb241c',
+  },
+  {
+    date: '2020-12-22',
+    title: 'Nannou で始める Rust / Creative-Coding',
+    siteName: 'Zenn',
+    link: 'https://zenn.dev/pvcresin/articles/4b9edacc87527a',
+  },
+
+  {
     date: '2020-12-17',
     title: 'eslint-loader の使用をやめることで、ビルド時間を短縮した話',
     siteName: 'Sansan Builders Blog',
@@ -66,6 +85,24 @@ const news: NewsData[] = [
     title: 'CSS について復習する',
     siteName: 'Sansan Builders Blog',
     link: 'https://buildersbox.corp-sansan.com/entry/2020/02/10/110000',
+  },
+  {
+    date: '2019-12-17',
+    title: 'Yew ではじめる Rust / WebAssembly 開発',
+    siteName: 'Qiita',
+    link: 'https://qiita.com/pvcresin/items/614cbd363af82e11c204',
+  },
+  {
+    date: '2017-10-03',
+    title: 'pugで書くRiot Routerの開発環境メモ',
+    siteName: 'Qiita',
+    link: 'https://qiita.com/pvcresin/items/2e139f32c8967dffdccd',
+  },
+  {
+    date: '2017-10-03',
+    title: 'pugで書くRiotの開発環境メモ',
+    siteName: 'Qiita',
+    link: 'https://qiita.com/pvcresin/items/3b49600720c8ecefedd4',
   },
 ]
 
