f61ccae61bedb3d39a6cd13449533a8157ffacde
pvcresin <pvcresin0730@gmail.com>
2022-05-08 18:41:22 +0900

    Add icon to navi

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { FaFolder, FaListUl, FaRegAddressCard, FaRegUser, FaUserAlt } from 'react-icons/fa'
 
 import styles from './navigation.module.scss'
 
@@ -8,38 +9,45 @@ const Navigation = () => {
   const { pathname } = router
   return (
     <nav className={styles.root}>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <Link href='/'>
-            <span className={pathname === '/' ? styles.linkActive : styles.link}>
-              Raika Toriyama
-            </span>
-          </Link>
-        </li>
-        <li className={styles.item}>
+      <div className={`container ${styles.container}`}>
+        <Link href='/'>
+          <span className={styles.item}>
+            <img
+              className={styles.icon}
+              src='/icon-pvcresin.svg'
+              alt='pvcresinのアイコン'
+              loading='lazy'
+            />
+            <span className={styles.logoText}>pvcresin</span>
+          </span>
+        </Link>
+        <ul>
           <Link href='/about'>
-            <span className={pathname === '/about' ? styles.linkActive : styles.link}>About</span>
+            <li className={pathname === '/about' ? styles.itemActive : styles.item}>
+              <FaUserAlt size='1.2rem' />
+              <span className={styles.linkText}>About</span>
+            </li>
           </Link>
-        </li>
-        <li className={styles.item}>
           <Link href='/projects'>
-            <span
+            <li
               className={
                 pathname === '/projects' || pathname === '/projects/[projectName]'
-                  ? styles.linkActive
-                  : styles.link
+                  ? styles.itemActive
+                  : styles.item
               }
             >
-              Projects
-            </span>
+              <FaFolder size='1.2rem' />
+              <span className={styles.linkText}>Projects</span>
+            </li>
           </Link>
-        </li>
-        <li className={styles.item}>
           <Link href='/news'>
-            <span className={pathname === '/news' ? styles.linkActive : styles.link}>News</span>
+            <li className={pathname === '/news' ? styles.itemActive : styles.item}>
+              <FaListUl size='1.2rem' />
+              <span className={styles.linkText}>News</span>
+            </li>
           </Link>
-        </li>
-      </ul>
+        </ul>
+      </div>
     </nav>
   )
 }
