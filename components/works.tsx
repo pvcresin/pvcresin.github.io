0d4eac38b4e6c3af7887d4bb42ff22a3b4276231
pvcresin <pvcresin0730@gmail.com>
2022-05-19 22:15:30 +0900

    Remove category

--- components/works.tsx
+++ components/works.tsx
@@ -1,32 +1,28 @@
 import Link from 'next/link'
 
-import worksArray, { Category } from 'data'
+import worksArray from 'data'
 
 import Webp from './webp'
 
 import styles from './works.module.scss'
 
-const initialCategory: Category = 'All'
-
-const Works: React.FC<{ category?: Category }> = ({ category }) => (
+const Works: React.FC = () => (
   <div className={styles.root}>
-    {worksArray
-      .filter((w) => category === initialCategory || w.category === category)
-      .map((w, index) => (
-        <div className={`pure-u-sm-1-2 pure-u-md-1-3 pure-u-1-1 ${styles.content}`} key={w.key}>
-          <Link href={`/works/${w.key}`}>
-            <article className={styles.card} role='link' tabIndex={100 + index}>
-              <div className={styles.imageWrapper}>
-                <Webp imageClassName={styles.image} src={w.img}></Webp>
-              </div>
-              <div className={styles.info}>
-                <h3 className={styles.title}>{w.title}</h3>
-                <p className={styles.description}>{w.sub}</p>
-              </div>
-            </article>
-          </Link>
-        </div>
-      ))}
+    {worksArray.map((w, index) => (
+      <div className={`pure-u-sm-1-2 pure-u-md-1-3 pure-u-1-1 ${styles.content}`} key={w.key}>
+        <Link href={`/works/${w.key}`}>
+          <article className={styles.card} role='link' tabIndex={100 + index}>
+            <div className={styles.imageWrapper}>
+              <Webp imageClassName={styles.image} src={w.img}></Webp>
+            </div>
+            <div className={styles.info}>
+              <h3 className={styles.title}>{w.title}</h3>
+              <p className={styles.description}>{w.sub}</p>
+            </div>
+          </article>
+        </Link>
+      </div>
+    ))}
   </div>
 )
 
--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -5,43 +5,17 @@ import Footer from 'components/footer'
 import Navigation from 'components/navigation'
 
 import styles from './index.module.scss'
-import { handlers } from 'utils/keyboardlistener'
-import { categories, Category } from 'data'
 
-const WorksPage = () => {
-  const [selectedCategory, setSelectedCategory] = React.useState<Category>(categories[0])
-
-  return (
-    <div className='root'>
-      <Navigation />
-      <section className={styles.section}>
-        <div className='container'>
-          <ul className={styles.buttons}>
-            {categories.map((c, index) => (
-              <a
-                onClick={() => setSelectedCategory(c)}
-                key={c}
-                role='link'
-                className={styles.link}
-                tabIndex={10 + index}
-                {...handlers}
-              >
-                <li
-                  className={`${styles.button}${
-                    selectedCategory === c ? ` ${styles.buttonSelected}` : ''
-                  }`}
-                >
-                  {c}
-                </li>
-              </a>
-            ))}
-          </ul>
-          <Works category={selectedCategory} />
-        </div>
-      </section>
-      <Footer />
-    </div>
-  )
-}
+const WorksPage = () => (
+  <div className='root'>
+    <Navigation />
+    <section className={styles.section}>
+      <div className='container'>
+        <Works />
+      </div>
+    </section>
+    <Footer />
+  </div>
+)
 
 export default WorksPage
