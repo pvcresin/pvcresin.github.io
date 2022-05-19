3dc3b573365b0635c8d422e3405b69483044ca7e
pvcresin <pvcresin0730@gmail.com>
2022-05-19 22:26:00 +0900

    Use li in ul

--- components/newslist.tsx
+++ components/newslist.tsx
@@ -7,7 +7,7 @@ export type NewsData = { date: string; at?: string; title: string; link?: string
 const NewsList: React.FC<{ newsData: NewsData[] }> = ({ newsData }) => (
   <ul className={styles.root}>
     {newsData.map((item) => (
-      <article className={`pure-g ${styles.item}`} key={item.title}>
+      <li className={`pure-g ${styles.item}`} key={item.title}>
         <time className={`pure-u-1-1 pure-u-lg-3-24 ${styles.date}`} dateTime={item.date}>
           {item.date}
         </time>
@@ -21,7 +21,7 @@ const NewsList: React.FC<{ newsData: NewsData[] }> = ({ newsData }) => (
           )}
           <span className={styles.at}>{item.at || ''}</span>
         </span>
-      </article>
+      </li>
     ))}
   </ul>
 )
