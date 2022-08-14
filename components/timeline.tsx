bb50b54fbf313ca88d88ca19740a77ea202c2e19
pvcresin <pvcresin0730@gmail.com>
2022-08-14 15:06:22 +0900

    Invert timeline

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -15,12 +15,20 @@ export type Item = {
 
 const items: Item[] = [
   {
-    startYear: '2013-04-01',
-    endYear: '2017-03-31',
-    title: '明治大学',
-    titleLinkUrl: 'https://www.meiji.ac.jp/',
+    startYear: '2019-04-01',
+    endYear: undefined,
+    title: 'Sansan 株式会社',
+    titleLinkUrl: 'https://jp.corp-sansan.com/',
     description:
-      '総合数理学部 先端メディアサイエンス学科  学士（理学）。\nモバイル / ウェアラブルデバイスのインタラクションに関する研究に従事。',
+      '2019年 4月新卒でSansan株式会社に入社。\nWebエンジニアとして、\n- Eightを基盤とした採用サービスの開発\n- Web FrontendのDevOpsの改善\nに従事。',
+  },
+  {
+    startYear: '2017-04-01',
+    endYear: '2019-03-31',
+    title: '明治大学 大学院',
+    titleLinkUrl: 'https://www.meiji.ac.jp/dai_in/faculty/02.html',
+    description:
+      '先端数理科学研究科 先端メディアサイエンス専攻。\n修士（工学）。\nHCI（Human-Computer Interaction）を専攻し、モバイル / ウェアラブルデバイスに関するインタラクションの研究を行う。',
   },
   {
     startYear: '2016-10-01',
@@ -31,20 +39,12 @@ const items: Item[] = [
       'フロントエンドエンジニア（インターン）。\n- 電子書籍・音声合成関連の研究用システムの開発\n- AIスピーカー・Felica・LINE botを用いたサービス開発\n- CMSを用いたWebサイト制作\nに従事。',
   },
   {
-    startYear: '2017-04-01',
-    endYear: '2019-03-31',
-    title: '明治大学 大学院',
-    titleLinkUrl: 'https://www.meiji.ac.jp/dai_in/faculty/02.html',
+    startYear: '2013-04-01',
+    endYear: '2017-03-31',
+    title: '明治大学',
+    titleLinkUrl: 'https://www.meiji.ac.jp/',
     description:
-      '先端数理科学研究科 先端メディアサイエンス専攻  修士（工学）。\nモバイル / ウェアラブルデバイスに関するインタラクションの研究に従事。',
-  },
-  {
-    startYear: '2019-04-01',
-    endYear: undefined,
-    title: 'Sansan 株式会社',
-    titleLinkUrl: 'https://jp.corp-sansan.com/',
-    description:
-      '2019年 4月新卒でSansan株式会社に入社。\nWebエンジニアとして、\n- Eightを基盤とした採用サービスの開発\n- Web FrontendのDevOpsの改善\nに従事。',
+      '総合数理学部 先端メディアサイエンス学科。\n学士（理学）。\nHCI（Human-Computer Interaction）を専攻し、モバイル / ウェアラブルデバイスのインタラクションに関する研究を行う。',
   },
 ]
 
