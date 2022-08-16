386464fd0c7dfbb5d4a4862448cc48a2566c6f1e
pvcresin <pvcresin0730@gmail.com>
2022-08-16 22:18:40 +0900

    Fix style

--- components/about.tsx
+++ components/about.tsx
@@ -5,6 +5,14 @@ import { Link } from '@components/link'
 import { FadeInSection } from './FadeInSection'
 import styles from './about.module.scss'
 
+const aboutText = [
+  'Web Frontend とデザインが好きなソフトウェアエンジニア。',
+  '大学・大学院では HCI（Human-Computer Interaction）を専攻。',
+  '2019年より Sansan 株式会社でフロントエンドエンジニアとして採用サービスの開発 / DX（Developer eXperience）の改善に従事。',
+  'Minimalism を大切にしており、Minimal な機能やデザインから最大限の UX を生み出すことを意識して、日々プロダクトと向き合っている。',
+  'エンジニアリングを通してプロダクトの UI / UX をデザインすることに興味があり、人と議論しながらモノ作りを行いたい。',
+].join('\n')
+
 export const About = () => (
   <div className={styles.root}>
     <div className='container'>
@@ -24,29 +32,13 @@ export const About = () => (
         <div className='pure-u-sm-1-24 pure-u-1-1'></div>
         <div className={`pure-u-sm-17-24 pure-u-1-1 ${styles.aboutTextContainer}`}>
           <FadeInSection>
-            <div className={styles.aboutText}>
-              {'Web Frontend とデザインが好きなソフトウェアエンジニア。'}
-              <br />
-              {'大学・大学院では HCI（Human-Computer Interaction）を専攻。'}
-              <br />
-              {
-                '2019年より Sansan 株式会社でフロントエンドエンジニアとして採用サービスの開発 / DX（Developer eXperience）の改善に従事。'
-              }
-              <br />
-              {
-                'Minimalism を大切にしており、Minimal な機能やデザインから最大限の UX を生み出すことを意識して、日々プロダクトと向き合っている。'
-              }
-              <br />
-              {
-                'エンジニアリングを通してプロダクトの UI / UX をデザインすることに興味があり、人と議論しながらモノ作りを行いたい。'
-              }
-            </div>
+            <div className={styles.aboutText}>{aboutText}</div>
             <ul>
               <li className={`pure-g ${styles.info}`}>
                 <div className={`pure-u-1-12 pure-u-lg-1-24 ${styles.infoIcon}`}>
                   <FaMapMarkerAlt />
                 </div>
-                <div className={`pure-u-11-12 ${styles.right}`}>Tokyo, Japan</div>
+                <div className={`pure-u-11-12 ${styles.right}`}>Tokyo / Kanagawa, Japan</div>
               </li>
               <li className={`pure-g ${styles.info}`}>
                 <div className={`pure-u-1-12 pure-u-lg-1-24 ${styles.infoIcon}`}>
