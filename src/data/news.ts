f87afb575a197566f965537fc452d7755f3f61f0
pvcresin <pvcresin0730@gmail.com>
2022-10-11 00:53:00 +0900

    Remove white space

--- src/data/news.ts
+++ src/data/news.ts
@@ -164,13 +164,13 @@ export const talks: NewsData[] = [
 
 export const interviews: NewsData[] = [
   {
-    date: ' 2022-07-25',
+    date: '2022-07-25',
     title: '「第2回 ツクってアソぶハッカソン」受賞者インタビュー③ 優秀賞',
     at: 'ツクってアソぶハッカソン',
     link: 'https://tsukuaso.com/news/3p1__r_e34ox',
   },
   {
-    date: ' 2019-10-03',
+    date: '2019-10-03',
     title: '【Sansan19卒新卒インタビューVol.02】新卒エンジニアに聞いたSansanへの入社理由。',
     at: 'Wantedly',
     link: 'https://www.wantedly.com/companies/Sansan/post_articles/189105',
