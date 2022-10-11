9aabf244c120dc5a5d7ca42cb7741e794e8aac23
pvcresin <pvcresin0730@gmail.com>
2022-10-11 19:22:56 +0900

    Fix text

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -8,7 +8,7 @@ import styles from './about.module.scss'
 const aboutText = [
   'Web Frontend とデザインが好きなソフトウェアエンジニア。',
   '大学・大学院では HCI（Human-Computer Interaction）を専攻。',
-  '2019年より Sansan 株式会社でフロントエンドエンジニアとして採用サービスの開発 / DX（Developer eXperience）の改善に従事。',
+  '2019年より Sansan 株式会社でフロントエンドエンジニアとして採用サービスの開発 / DX（Developer Experience）の改善に従事。',
   'Minimalism を大切にしており、Minimal な機能やデザインから最大限の UX を生み出すことを意識して、日々プロダクトと向き合っている。',
   'エンジニアリングを通してプロダクトの UI / UX をデザインすることに興味があり、人と議論しながらモノ作りを行いたい。',
 ].join('\n')
