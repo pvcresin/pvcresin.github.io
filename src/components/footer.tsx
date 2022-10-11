8ca4e3a75c168d3c3b2dac6ce3857243cb657be5
pvcresin <pvcresin0730@gmail.com>
2022-10-11 19:21:17 +0900

    FIx footer style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,10 @@
-import { Social } from '@components/social'
-
 import styles from './footer.module.scss'
 
 export const Footer = () => {
   return (
     <div className={styles.root}>
       <div className={`container ${styles.container}`}>
-        <Social />
-        <p className={styles.copyright}>© pvcresin</p>
+        <p className={styles.copyright}>© 2015-{new Date().getFullYear()} pvcresin</p>
       </div>
     </div>
   )
