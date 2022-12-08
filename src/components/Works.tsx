ee36c7f266ef9b8a7dd210d06a5db5c9fa265c26
pvcresin <pvcresin0730@gmail.com>
2022-12-08 23:42:37 +0900

    Fix card focus

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -12,16 +12,14 @@ export const Works: React.FC = () => (
   <div className={styles.root}>
     {worksArray.map((w) => (
       <div className={styles.content} key={w.key}>
-        <Link href={`/works/${w.key}`}>
-          <article className={styles.card} role='link' tabIndex={0} {...handlers}>
-            <FadeInSection>
-              <div className={styles.imageWrapper}>
-                <Webp imageClassName={styles.image} src={w.img}></Webp>
-              </div>
-              <h3 className={styles.title}>{w.title}</h3>
-              <p className={styles.description}>{w.sub}</p>
-            </FadeInSection>
-          </article>
+        <Link href={`/works/${w.key}`} className={styles.card} tabIndex={0} {...handlers}>
+          <FadeInSection>
+            <div className={styles.imageWrapper}>
+              <Webp imageClassName={styles.image} src={w.img}></Webp>
+            </div>
+            <h3 className={styles.title}>{w.title}</h3>
+            <p className={styles.description}>{w.sub}</p>
+          </FadeInSection>
         </Link>
       </div>
     ))}
