917733c9bd97b238afe14bbbfc7b42b45571221e
pvcresin <pvcresin0730@gmail.com>
2022-12-31 16:15:06 +0900

    Add locale toolbar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,18 @@
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    defaultValue: 'ja',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'ja', right: '🇯🇵', title: '日本語' },
+        { value: 'en', right: '🇺🇸', title: 'English' },
+      ],
+    },
+  },
+}
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
