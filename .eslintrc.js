0151f8890f73a18aa953bd4ed69fed7565dee236
pvcresin <pvcresin0730@gmail.com>
2022-12-31 15:50:09 +0900

    Integrate storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ const config = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
