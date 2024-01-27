c6a8d1eb280b05592600c7dbfbb47014de789ae1
pvcresin <pvcresin0730@gmail.com>
2024-01-27 23:59:25 +0900

    refactor: migrate not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,21 @@
+'use client'
+
 import { Footer } from '@/components/Footer'
 import { Navigation } from '@/components/Navigation'
 import { WidthContainer } from '@/components/WidthContainer'
 
-import styles from './404.module.scss'
+import type { Metadata } from 'next'
+
+import styles from './not-found.module.scss'
+
+export const metadata = { title: 'Page Not Found' } satisfies Metadata
 
 const NotFoundPage = () => (
   <div className='root'>
     <Navigation />
     <section className={styles.content}>
       <WidthContainer>
-        <h1>Page Not Found</h1>
+        <h1>{metadata.title}</h1>
       </WidthContainer>
     </section>
     <Footer />
